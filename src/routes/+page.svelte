a39587636f02ec0a274cf47a33ab181936e86e9e
Jonathan Wu <jonathan.wu3@outlook.com>
2026-04-19 15:41:51 -0400

    branch

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import type { PageProps } from "./$types";
+
+    let { data } = $props();
+
+
+    const color = $derived(`rgb(${data.data.join(",")})`);
+</script>
+
+<div class="p-[10px]" style="background-color: {color}">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visisdfsdft <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
